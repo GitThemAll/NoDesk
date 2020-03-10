e18e11b16c32506c9c0e258abd40e4a74215a575
Abdul SH <Abdul@talmark.nl>
2020-03-11 00:43:07 +0100

    .

---
+++ NoDesk/DAL/Database.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+using MongoDB.Bson;
+
+namespace NoDesk
+{
+	class Database
+	{
+		MongoClient client;
+		public Database()
+		{
+			this.client = new MongoClient(string.Format("mongodb+srv://{0}:{1}@{2}/test?retryWrites=true&w=majority",Settings.username, Settings.password, Settings.clusterAddress));
+
+
+		}
+
+	}
+}
--- NoDesk/View/NoDesk.cs
+++ NoDesk/View/NoDesk.cs
@@ -15,6 +15,9 @@ namespace NoDesk
 		public NoDesk()
 		{
 			InitializeComponent();
+			
 		}
+		
+
 	}
 }
